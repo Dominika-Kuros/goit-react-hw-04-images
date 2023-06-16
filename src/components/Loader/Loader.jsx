17632f9b1b70c2fd871aeff145881eb78d27d8d9
Dominika-Kuros <113472378+Dominika-Kuros@users.noreply.github.com>
2023-06-16 17:58:04 +0200

    fixing loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,14 +1,17 @@
 import { Blocks } from 'react-loader-spinner';
+import css from "./Loader.module.css";
 
 export const Loader = () => {
   return (
-    <Blocks
-      visible={true}
-      height="80"
-      width="80"
-      ariaLabel="blocks-loading"
-      wrapperStyle={{}}
-      wrapperClass="blocks-wrapper"
-    />
+    <div className={css.loader}>
+      <Blocks
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+      />
+    </div>
   );
 };
